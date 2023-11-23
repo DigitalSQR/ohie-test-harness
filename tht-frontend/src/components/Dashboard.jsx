06ee45cddd55697d9ae96b0158e981070f158454
Shinu Thomas <144095318+Shinu-Thomas@users.noreply.github.com>
2023-11-23 10:55:10 +0530

    ﻿Ts Changes.

--- tht-frontend/src/components/Dashboard.jsx
+++ tht-frontend/src/components/Dashboard.jsx
@@ -1,25 +1,25 @@
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 export default function Dashboard() {
   //the below block of code is redundant as component rendering will be done by routing.
-  const [activeComponent,setActiveComponent]=useState(null);
+  // const [activeComponent,setActiveComponent]=useState(null);
 
-  const renderActiveComponent=()=>{
+  // const renderActiveComponent=()=>{
     
-    // console.log(activeComponent)
-    switch(activeComponent){
-      case'User':
-      console.log('rendering user')
-        //  return <User/>; //rendering based on routes
-      case'TestCases':
-        return <TestCases/>;
-        default:
-          return null;
+  //   // console.log(activeComponent)
+  //   switch(activeComponent){
+  //     case'User':
+  //     console.log('rendering user')
+  //       //  return <User/>; //rendering based on routes
+  //     case'TestCases':
+  //       return <TestCases/>;
+  //       default:
+  //         return null;
 
-    }
-  }
+  //   }
+  // }
   //the redundant block ends here.
   return (
    <Fragment>
