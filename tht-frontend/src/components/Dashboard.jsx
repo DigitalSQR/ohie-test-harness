99b816157a7b67bdbe42d97885820fe477f5826d
harshp@argusoft.in <harshp@argusoft.in>
2024-01-10 13:45:18 +0530

    Sidemenu toogle issue css fix

--- tht-frontend/src/components/Dashboard.jsx
+++ tht-frontend/src/components/Dashboard.jsx
@@ -1,40 +1,20 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 // import "../styles/Dashboard.css";
 export default function Dashboard() {
-  //the below block of code is redundant as component rendering will be done by routing.
-  // const [activeComponent,setActiveComponent]=useState(null);
-
-  // const renderActiveComponent=()=>{
-
-  //   // console.log(activeComponent)
-  //   switch(activeComponent){
-  //     case'User':
-  //     console.log('rendering user')
-  //       //  return <User/>; //rendering based on routes
-  //     case'TestCases':
-  //       return <TestCases/>;
-  //       default:
-  //         return null;
-
-  //   }
-  // }
-  //the redundant block ends here.
   return (
     <Fragment>
       <Header />
-      <div style={{ display: "flex" }}>
-        {/* <Sidebar onComponentClick={(component)=>{
-        setActiveComponent(component)}}/> */}
+      <div>
         <Sidebar />
-        <main style={{ flex: 1 }}>{<Outlet />}
-         
+        <main>
+          {<Outlet />}
         </main>
       </div>
-      <Footer/>
+      <Footer />
     </Fragment>
   );
 }
--- tht-frontend/src/components/Landing.jsx
+++ tht-frontend/src/components/Landing.jsx
@@ -1,30 +1,27 @@
 import { useNavigate } from "react-router-dom";
 import tool_icon from "../styles/images/tool-icon.png";
-export default function Landing(){
-    const navigate=useNavigate();
-    return(
-        <div id="wrapper">
+import { useEffect } from "react";
+export default function Landing() {
+  const navigate = useNavigate();
+
+
+  return (
+    <div id="wrapper">
         <div class="col-lg-6 mx-auto text-center pt-5">
-          <img src={tool_icon} />
-          <h4 class="mt-2">Testing Harness Tool</h4>
-          <p class="font-size-16 mt-4">
-            Register your application to open-source testing harness and
-            complete test framework that will facilitate testing how well
-            technologies align to the OpenHIE Architecture specification and
-            health and data content, as specified by WHO SMART Guidelines.
-          </p>
-          <p class="my-4">
-            <a class="text-blue font-weight-500" href="https://guides.ohie.org/arch-spec/openhie-component-specifications-1">
-              View OpenHIE Component Specifications
-            </a>
-          </p>
-          <div class="my-4">
-            {" "}
-            <button class="btn btn-primary btn-blue mt-2" onClick={()=>{navigate("/dashboard/choose-test")}}>
-              <i class="bi bi-pencil-square"></i> Register application
-            </button>
-          </div>
+            <img src={tool_icon} />
+            <h4 class="mt-2">Testing Harness Tool</h4>
+            <p class="font-size-16 mt-4">
+                Register your application to open-source testing harness and complete test framework that will
+                facilitate testing how well technologies align
+                to the OpenHIE Architecture specification and health and data content, as specified by WHO SMART
+                Guidelines.
+            </p>
+            <p class="my-4"><a class="text-blue font-weight-500" target="_blank"
+                    href="https://guides.ohie.org/arch-spec/openhie-component-specifications-1">View OpenHIE Component
+                    Specifications</a></p>
+            <div class="my-4"> <button class="btn btn-primary btn-blue mt-2" onClick={() => { navigate("/dashboard/choose-test") }}><i class="bi bi-pencil-square"></i>
+                    Register application</button></div>
         </div>
-      </div>
-    )
+    </div>
+  )
 }
