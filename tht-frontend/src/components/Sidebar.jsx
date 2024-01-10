0faa350cbb7e88c960b4d0458b3d05bf31591276
harshp@argusoft.in <harshp@argusoft.in>
2024-01-10 16:17:47 +0530

    removed css issue with navigate when sidebar is collapsed

--- tht-frontend/src/components/Sidebar.jsx
+++ tht-frontend/src/components/Sidebar.jsx
@@ -6,12 +6,13 @@ import { clearAuthInfo } from '../api/configs/axiosConfigs'
 import "../scss/_sidebar.scss";
 import logo from "../styles/images/logo-white.png"
 import { useEffect, useState } from 'react';
+
 export default function Sidebar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const[activeMenuItem,setActiveMenuItem] = useState();
+  const [activeMenuItem, setActiveMenuItem] = useState();
 
   /* 
    * This is to expand/shrink the wrapper when the side menu bar is toogled
@@ -21,7 +22,9 @@ export default function Sidebar() {
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
     let wrapper = document.getElementById("wrapper");
-    wrapper.classList.toggle("expand");
+    if (wrapper) {
+      wrapper.classList.toggle("expand");
+    }
   };
 
   const handleMenuItemClick = (path) => {
@@ -29,51 +32,54 @@ export default function Sidebar() {
     navigate(path);
   };
 
-  useEffect(()=>{
-    setActiveMenuItem(location.pathname)
-  },[location])
- 
+  useEffect(() => {
+    setActiveMenuItem(location.pathname);
+    let wrapper = document.getElementById("wrapper");
+    if (wrapper) {
+      isSidebarOpen ? wrapper.classList.remove("expand") : wrapper.classList.add("expand");
+    }
+  }, [location]);
 
   return (
-      <div className={isSidebarOpen ? "sidebar-wrapper open" : "sidebar-wrapper shrink"} id="mySidenav">
-          <div className="close-sidemenu-icon" onClick={toggleSidebar}>
-              <i className="bi bi-filter-left"></i>
-          </div>
-          <div className="logo-white">
-              <img src={logo} alt="Logo" />
-          </div>
-          <ul className="side-menu">
-              <li>
-                  <a className={activeMenuItem === "/dashboard" ? "active" : ""} onClick={() => {handleMenuItemClick("/dashboard")}}>
-                      <i className="bi bi-columns-gap menu-left-icon"></i>
-                      <span> Dashboard </span>
-                  </a>
-              </li>
-              <li>
-                  <a className={activeMenuItem === "/dashboard/application-status" ? "active" : ""} onClick={() => handleMenuItemClick("/dashboard/application-status")}>
-                      <i className="bi bi-file-earmark-bar-graph menu-left-icon"></i>
-                      <span> Application Status </span>
-                  </a>
-              </li>
-              <li>
-                  <a className={activeMenuItem === "/dashboard/registration-request" ? "active" : ""} onClick={() => handleMenuItemClick("/dashboard/registration-request")}>
-                      <i className="bi bi-columns-gap menu-left-icon"></i>
-                      <span> Registration Request </span>
-                  </a>
-              </li>
-              <li>
-                  <a className={activeMenuItem === "/dashboard/application-request" ? "active" : ""} onClick={() => handleMenuItemClick("/dashboard/application-request") }>
-                      <i className="bi bi-file-earmark-bar-graph menu-left-icon"></i>
-                      <span> Application Request </span>
-                  </a>
-              </li>
-              <li>
-                  <a onClick={() => { dispatch(log_out()); }}>
-                      <i className="bi bi-file-earmark-bar-graph menu-left-icon"></i>
-                      <span> Logout </span>
-                  </a>
-              </li>
-          </ul>
+    <div className={isSidebarOpen ? "sidebar-wrapper open" : "sidebar-wrapper shrink"} id="mySidenav">
+      <div className="close-sidemenu-icon" onClick={toggleSidebar}>
+        <i className="bi bi-filter-left"></i>
       </div>
+      <div className="logo-white">
+        <img src={logo} alt="Logo" />
+      </div>
+      <ul className="side-menu">
+        <li>
+          <a className={activeMenuItem === "/dashboard" ? "active" : ""} onClick={() => { handleMenuItemClick("/dashboard") }}>
+            <i className="bi bi-columns-gap menu-left-icon"></i>
+            <span> Dashboard </span>
+          </a>
+        </li>
+        <li>
+          <a className={activeMenuItem === "/dashboard/application-status" ? "active" : ""} onClick={() => handleMenuItemClick("/dashboard/application-status")}>
+            <i className="bi bi-file-earmark-bar-graph menu-left-icon"></i>
+            <span> Application Status </span>
+          </a>
+        </li>
+        <li>
+          <a className={activeMenuItem === "/dashboard/registration-request" ? "active" : ""} onClick={() => handleMenuItemClick("/dashboard/registration-request")}>
+            <i className="bi bi-columns-gap menu-left-icon"></i>
+            <span> Registration Request </span>
+          </a>
+        </li>
+        <li>
+          <a className={activeMenuItem === "/dashboard/application-request" ? "active" : ""} onClick={() => handleMenuItemClick("/dashboard/application-request")}>
+            <i className="bi bi-file-earmark-bar-graph menu-left-icon"></i>
+            <span> Application Request </span>
+          </a>
+        </li>
+        <li>
+          <a onClick={() => { dispatch(log_out()); }}>
+            <i className="bi bi-file-earmark-bar-graph menu-left-icon"></i>
+            <span> Logout </span>
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
