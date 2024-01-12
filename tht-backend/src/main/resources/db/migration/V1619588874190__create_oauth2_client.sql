acde2c72962b8ac52597ab56ac2df74b0d01e03a
Bhavim09 <bmehta@argusoft.com>
2024-01-12 12:07:30 +0530

    #5 #6 #12 #13 Updated migration script to create CR testcases data.

--- tht-backend/src/main/resources/db/migration/V1619588874190__create_oauth2_client.sql
+++ tht-backend/src/main/resources/db/migration/V1619588874190__create_oauth2_client.sql
@@ -3,28 +3,31 @@
 --@author dhruv
 --@since 2023-09-13
 
-INSERT INTO oauth_client_details(
-    client_id,
-    resource_ids,
-    client_secret,
-    scope,
-    authorized_grant_types,
-    web_server_redirect_uri,
-    authorities,
-    access_token_validity,
-    refresh_token_validity,
-    additional_information,
-    autoapprove
-    ) VALUES (
-    'tht',
-    'resource_id',
-    '6ac2c7f6-9032-4d36-831f-2cc3a7fa910c',
-    'write',
-    'password,authorization_code,refresh_token,implicit',
-    null,
-    null,
-    60,
-    300,
-    null,
-    true
-);
+INSERT INTO
+    oauth_client_details(
+        client_id,
+        resource_ids,
+        client_secret,
+        scope,
+        authorized_grant_types,
+        web_server_redirect_uri,
+        authorities,
+        access_token_validity,
+        refresh_token_validity,
+        additional_information,
+        autoapprove
+    )
+VALUES
+    (
+        'tht',
+        'resource_id',
+        '6ac2c7f6-9032-4d36-831f-2cc3a7fa910c',
+        'write',
+        'password,authorization_code,refresh_token,implicit',
+        null,
+        null,
+        60,
+        300,
+        null,
+        true
+    );
