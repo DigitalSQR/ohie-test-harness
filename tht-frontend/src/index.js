ae2b077cc23faaa9323b1568f325bc7562e843be
Shinu Thomas <144095318+Shinu-Thomas@users.noreply.github.com>
2024-01-16 15:47:57 +0530

    remove strict mode

--- tht-frontend/src/index.js
+++ tht-frontend/src/index.js
@@ -25,13 +25,13 @@ if (persistedStateJSON) {
   }
 }
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>
-</React.StrictMode>
+// </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
