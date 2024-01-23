f9d9197a8e274719e9b4a554706388b5ec499d83
Anshik Thakur <athakur@argusoft.com>
2024-01-23 07:00:52 +0000

    pagination and sorting added for user registration page

--- tht-frontend/src/api/UserAPI.js
+++ tht-frontend/src/api/UserAPI.js
@@ -1,62 +1,71 @@
 import api from "./configs/axiosConfigs";
 
 export const UserAPI = {
-	viewUser: async function () {
-		try {
-			const response = await api.request({
-				url: `/user/principal`,
-				method: "GET",
-			});
-			return response.data;
-		} catch (error) {
-			throw error; // You can choose to re-throw the error or handle it in a specific way
-		}
-	},
-	getUsers: async function () {
-		try {
-			const response = await api.request({
-				url: `/user`,
-				method: "GET",
-			});
-			return response.data;
-		} catch (error) {
-			throw error; // You can choose to re-throw the error or handle it in a specific way
-		}
-	},
-	changeState: async function (userId, state) {
-		console.log("userapi " + userId, state);
-		try {
-			const response = await api.request({
-				url: `/user/state/${userId}/${state}`,
-				method: "PUT"
-			});
-			return response;
-		}
-		catch (error) { throw error }
-	},
-	getUserByState: async function (state) {
-		try {
-			const response = await api.request({
-				url: `/user`,
-				method: "GET",
-				params: {
-					state
-				}
-			});
-			return response.data;
-		} catch (error) {
-			throw error; // You can choose to re-throw the error or handle it in a specific way
-		}
-	},
-	getUserById: async function (userId) {
-		try {
-			const response = await api.request({
-				url: `/user/${userId}`,
-				method: "GET",
-			});
-			return response.data;
-		} catch (error) {
-			throw error; // You can choose to re-throw the error or handle it in a specific way
-		}
-	},
+  viewUser: async function () {
+    try {
+      const response = await api.request({
+        url: `/user/principal`,
+        method: "GET",
+      });
+      return response.data;
+    } catch (error) {
+      throw error; // You can choose to re-throw the error or handle it in a specific way
+    }
+  },
+  getUsers: async function () {
+    try {
+      const response = await api.request({
+        url: `/user`,
+        method: "GET",
+      });
+      return response.data;
+    } catch (error) {
+      throw error; // You can choose to re-throw the error or handle it in a specific way
+    }
+  },
+  changeState: async function (userId, state) {
+    console.log("userapi " + userId, state);
+    try {
+      const response = await api.request({
+        url: `/user/state/${userId}/${state}`,
+        method: "PUT",
+      });
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  },
+  getUserByState: async function (
+    sortFieldName = "createdAt",
+    sortDirection = "desc",
+    pageNumber = 1,
+    pageSize = 10
+  ) {
+    try {
+      console.log(sortFieldName, sortDirection);
+      const response = await api.request({
+        url: `/user`,
+        method: "GET",
+        params: {
+          sort: `${sortFieldName},${sortDirection}`,
+          page: pageNumber - 1,
+          size: pageSize,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      throw error; // You can choose to re-throw the error or handle it in a specific way
+    }
+  },
+  getUserById: async function (userId) {
+    try {
+      const response = await api.request({
+        url: `/user/${userId}`,
+        method: "GET",
+      });
+      return response.data;
+    } catch (error) {
+      throw error; // You can choose to re-throw the error or handle it in a specific way
+    }
+  },
 };
