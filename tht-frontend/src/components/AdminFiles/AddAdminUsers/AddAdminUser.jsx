f05bf9fb4de0afa3de4db6110284968f249c021a
Nitin Goyal <ngoyal@argusoft.com>
2024-02-08 07:44:39 +0000

    #179 Form in addAdmin Users And updateAdmin users using formik

--- tht-frontend/src/components/AdminFiles/AddAdminUsers/AddAdminUser.jsx
+++ tht-frontend/src/components/AdminFiles/AddAdminUsers/AddAdminUser.jsx
@@ -1,39 +1,54 @@
-import React, { useState } from "react";
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
 import "./addadminuser.scss";
 import { notification } from "antd";
 import { AdminUserAPI } from "../../../api/AdminUserAPI";
 import { useLoader } from "../../loader/LoaderContext";
+
 const AddAdminUser = () => {
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({
+  const { showLoader, hideLoader } = useLoader();
+
+  const initialValues = {
     name: "",
     email: "",
     password: "",
     roleIds: "",
-  });
-  const { showLoader, hideLoader } = useLoader();
-
-  const handleChange = (e) => {
-    const { id, value } = e.target;
-    setFormData({ ...formData, [id]: value });
   };
-  const handleSubmit = async () => {
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Name is required *"),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required *"),
+    password: Yup.string()
+      .required("Password is required *")
+      .min(6, "Password must be of minimum 6 characters"),
+    roleIds: Yup.string().required("Role is required *"),
+  });
+
+  const handleSubmit = async (values, { setSubmitting }) => {
     showLoader();
-    const body = { ...formData, roleIds: ["role." + formData.roleIds]};
-    AdminUserAPI.addUser(body).then(
-      (response) => {
+    const body = { ...values, roleIds: ["role." + values.roleIds] };
+    AdminUserAPI.addUser(body)
+      .then((response) => {
         hideLoader();
         notification.success({
           placement: "bottomRight",
           description: `User Added Successfully`,
         });
         navigate("/dashboard/admin-users");
-      },
-      (response) => {
+      })
+      .catch((error) => {
         hideLoader();
-        if (response.response.data && response.response.data.length > 0) {
-          response.response.data.forEach((element, index) => {
+        if (
+          error.response &&
+          error.response.data &&
+          error.response.data.length > 0
+        ) {
+          error.response.data.forEach((element, index) => {
             notification.error({
               placement: "bottomRight",
               description: `${element.message}`,
@@ -42,113 +57,133 @@ const AddAdminUser = () => {
         } else {
           notification.error({
             placement: "bottomRight",
-            description: `${response.response.data[0].message}`,
+            description: `${error.message}`,
           });
         }
-      }
-    );
+      })
+      .finally(() => {
+        setSubmitting(false);
+      });
   };
+
   return (
     <div>
       <div id="wrapper">
         <div className="col-lg-9 col-xl-7 col-xxl-5 col-md-11 mx-auto pt-5">
           <div className="form-bg-white">
             <span className="heading-line-up">Admin User Registration</span>
-            <div className="row">
-              <div className="col-12">
-                <div className="custom-input mb-3">
-                  <label htmlFor="name" className="form-label">
-                    Name
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    placeholder="Your Name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    autoComplete="off"
-                  />
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
+            >
+              <Form>
+                <div className="row">
+                  <div className="col-12">
+                    <div className="custom-input mb-3">
+                      <label htmlFor="name" className="form-label">
+                        Name
+                      </label>
+                      <Field
+                        type="text"
+                        id="name"
+                        name="name"
+                        className="form-control"
+                      />
+                      <ErrorMessage
+                        name="name"
+                        component="div"
+                        className="error-message"
+                      />
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
 
-            <div className="row">
-              <div className="col-12">
-                <div className="custom-input mb-3">
-                  <label htmlFor="email" className="form-label">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="email"
-                    placeholder="Your Email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    autoComplete="off"
-                  />
+                <div className="row">
+                  <div className="col-12">
+                    <div className="custom-input mb-3">
+                      <label htmlFor="email" className="form-label">
+                        Email
+                      </label>
+                      <Field
+                        type="email"
+                        id="email"
+                        name="email"
+                        className="form-control"
+                      />
+                      <ErrorMessage
+                        name="email"
+                        component="div"
+                        className="error-message"
+                      />
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
 
-            <div className="row">
-              <div className="col-12">
-                <div className="custom-input mb-3">
-                  <label htmlFor="password" className="form-label">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    placeholder="Your Password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    autoComplete="off"
-                  />
+                <div className="row">
+                  <div className="col-12">
+                    <div className="custom-input mb-3">
+                      <label htmlFor="password" className="form-label">
+                        Password
+                      </label>
+                      <Field
+                        type="password"
+                        id="password"
+                        name="password"
+                        className="form-control"
+                      />
+                      <ErrorMessage
+                        name="password"
+                        component="div"
+                        className="error-message"
+                      />
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
 
-            <div className="row">
-              <div className="col-12">
-                <div className="custom-input mb-3">
-                  <label htmlFor="role" className="form-label">
-                    Role
-                  </label>
-                  <select
-                    className="form-select"
-                    id="roleIds"
-                    value={formData.roleIds}
-                    onChange={handleChange}
+                <div className="row">
+                  <div className="col-12">
+                    <div className="custom-input mb-3">
+                      <label htmlFor="role" className="form-label">
+                        Role
+                      </label>
+                      <Field
+                        as="select"
+                        id="roleIds"
+                        name="roleIds"
+                        className="form-select"
+                      >
+                        <option value="">Select Role</option>
+                        <option value="tester">Tester</option>
+                        <option value="admin">Admin</option>
+                      </Field>
+                      <ErrorMessage
+                        name="roleIds"
+                        component="div"
+                        className="error-message"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="my-4 text-end">
+                  <button
+                    className="btn btn-primary btn-white py-2 font-size-14"
+                    onClick={() => {
+                      navigate("/dashboard/admin-users");
+                    }}
                   >
-                    <option value="#" >
-                      Select Role
-                    </option>
-                    <option value="tester">Tester</option>
-                    <option value="admin">Admin</option>
-                  </select>
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-blue btn-submit py-2 font-size-14"
+                  >
+                    Submit
+                  </button>
                 </div>
-              </div>
-            </div>
-
-            <div className="my-4 text-end">
-              <button
-                className="btn btn-primary btn-white py-2 font-size-14"
-                onClick={() => {
-                  navigate("/dashboard/admin-users");
-                }}
-              >
-                Cancel
-              </button>
-              <button
-                className="btn btn-primary btn-blue btn-submit py-2 font-size-14"
-                onClick={handleSubmit}
-              >
-                Submit
-              </button>
-            </div>
+              </Form>
+            </Formik>
           </div>
         </div>
       </div>
