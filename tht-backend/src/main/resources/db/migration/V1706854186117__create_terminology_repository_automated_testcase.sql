2db81b2148e7b575193e34c7a239a937963024c8
Ishita Shaneshah <ishaneshah@argusoft.com>
2024-02-09 06:59:17 +0000

    created test case for tswf1

---
+++ tht-backend/src/main/resources/db/migration/V1706854186117__create_terminology_repository_automated_testcase.sql
@@ -0,0 +1,84 @@
+    INSERT INTO component (
+       	    id,
+        	name,
+        	description,
+        	state,
+        	rank,
+        	created_by,
+            updated_by,
+            created_at,
+            updated_at,
+            version
+        ) VALUES (
+            'component.terminology.service',
+            'Terminology Service',
+            'Terminology Service Component (https://guides.ohie.org/arch-spec/openhie-component-specifications-1/openhie-terminology-service-ts)',
+            'component.status.active',
+            10,
+            'ivasiwala@argusoft.com',
+            'ivasiwala@argusoft.com',
+            Now(),
+            Now(),
+            0
+    );
+
+
+INSERT INTO specification (
+       	    id,
+        	name,
+        	description,
+        	state,
+        	rank,
+        	is_functional,
+        	is_required,
+        	component_id,
+        	created_by,
+            updated_by,
+            created_at,
+            updated_at,
+            version
+        ) VALUES (
+            'specification.ts.tswf.1',
+            'TSWF-1',
+            'TSWF-1 Specification of the Terminology Service Component (https://guides.ohie.org/arch-spec/introduction/terminology-service-workflow/verify-code-existence)',
+            'specification.status.active',
+            1,
+            false,
+            true,
+            'component.terminology.service',
+            'ivasiwala@argusoft.com',
+            'ivasiwala@argusoft.com',
+            Now(),
+            Now(),
+            0
+        );
+
+    INSERT INTO testcase (
+       	    id,
+        	name,
+        	description,
+        	state,
+        	rank,
+        	is_manual,
+        	bean_name,
+        	specification_id,
+        	created_by,
+            updated_by,
+            created_at,
+            updated_at,
+            version
+        ) VALUES (
+            'testcase.ts.tswf.1.1',
+            'Verify Code Existence',
+            'Testcase to Verify Code Existence for specification TSWF1 of the terminology service',
+            'testcase.status.active',
+            1,
+            false,
+            'TSWF1TestCase1',
+            'specification.ts.tswf.1',
+            'ivasiwala@argusoft.com',
+            'ivasiwala@argusoft.com',
+            Now(),
+            Now(),
+            0
+        );
