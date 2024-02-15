551e6889c2a1073422fb5514cd6fe11d6d1769f0
PATELAEX <dhruv.patel.3@bc.edu>
2024-02-15 17:03:34 +0530

    Updated Document Types.

--- tht-frontend/src/constants/document_constants.js
+++ tht-frontend/src/constants/document_constants.js
@@ -5,10 +5,13 @@ export const DOCUMENT_STATE_INACTIVE = "document.status.inactive";
 export const DOCUMENT_STATE_ACTIVE = "document.status.active";
 
 export const DOCUMENT_TYPE_FOR_USER = {
-    PROFILE_PICTURE: "document.type.profilePicture"
+    PROFILE_PICTURE: "document.type.user.profilePicture"
 }
 
 export const DOCUMENT_TYPE_FOR_TEST_CASE_RESULTS = {
-    DOCUMENT_TYPE_EVIDENCE: "document.type.evidence",
-    DOCUMENT_TYPE_EVIDENCEINTEXT: "document.type.evidenceInText"
+    DOCUMENT_TYPE_EVIDENCE: "document.type.testcaseresult.evidence",
+}
+
+export const DOCUMENT_TYPE_FOR_TEST_CASES = {
+    DOCUMENT_TYPE_QUESTION: "document.type.testcase.question"
 }
