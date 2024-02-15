cad91bdac48ed9cd405351eb4061366ca54362b0
PATELAEX <dhruv.patel.3@bc.edu>
2024-02-15 17:03:34 +0530

    Updated Document Types.

--- tht-backend/src/main/java/com/argusoft/path/tht/fileservice/constant/DocumentTypeConstants.java
+++ tht-backend/src/main/java/com/argusoft/path/tht/fileservice/constant/DocumentTypeConstants.java
@@ -10,23 +10,27 @@ import java.util.Set;
 public class DocumentTypeConstants {
 
     public static Map<String, Set<String>> validDocumentTypes;
-    public static final String DOCUMENT_TYPE_PROFILE_PICTURE = "document.type.profilePicture";
-    public static final String DOCUMENT_TYPE_EVIDENCE = "document.type.evidence";
-    public static final String DOCUMENT_TYPE_EVIDENCEINTEXT = "document.type.evidenceInText";
+    public static final String DOCUMENT_TYPE_PROFILE_PICTURE = "document.type.user.profilePicture";
+    public static final String DOCUMENT_TYPE_EVIDENCE = "document.type.testcaseresult.evidence";
+    public static final String DOCUMENT_TYPE_QUESTION = "document.type.testcase.question";
 
     public static Set<String> docTypeForUser = new HashSet<>(Set.of(
             DOCUMENT_TYPE_PROFILE_PICTURE
     ));
 
     public static Set<String> docTypeForTestCaseResult = new HashSet<>(Set.of(
-            DOCUMENT_TYPE_EVIDENCE,
-            DOCUMENT_TYPE_EVIDENCEINTEXT
+            DOCUMENT_TYPE_EVIDENCE
+    ));
+
+    public static Set<String> docTypeForTestCase = new HashSet<>(Set.of(
+            DOCUMENT_TYPE_QUESTION
     ));
 
     static {
         validDocumentTypes = Map.ofEntries(
                 Map.entry(UserServiceConstants.USER_REF_OBJ_URI, docTypeForUser),
-                Map.entry(TestcaseResultServiceConstants.TESTCASE_RESULT_REF_OBJ_URI, docTypeForTestCaseResult)
+                Map.entry(TestcaseResultServiceConstants.TESTCASE_RESULT_REF_OBJ_URI, docTypeForTestCaseResult),
+                Map.entry(TestcaseServiceConstants.TESTCASE_REF_OBJ_URI, docTypeForTestCase)
         );
     }
 
