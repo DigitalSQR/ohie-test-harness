4b7ccead65fd0ebc8c21607831ea4cd4cb329813
IshitaShaneshah <139424769+IshitaShaneshah@users.noreply.github.com>
2024-03-15 18:42:25 +0530

    #issue10 adding credentials in application.yml

--- tht-backend/src/main/java/com/argusoft/path/tht/systemconfiguration/constant/Constant.java
+++ tht-backend/src/main/java/com/argusoft/path/tht/systemconfiguration/constant/Constant.java
@@ -22,33 +22,22 @@ public final class Constant {
     public static final String ANONYMOUS_USER_NAME = "ANONYMOUS_USER";
     // Constants for validating
     public static final String CREATE_VALIDATION = "create.validation";
+
     public static final String UPDATE_VALIDATION = "update.validation";
     public static final String SUBMIT_VALIDATION = "submit.validation";
     public static final String START_PROCESS_VALIDATION = "start.process.validation";
     public static final String STOP_PROCESS_VALIDATION = "stop.process.validation";
     public static final String RESET_PROCESS_VALIDATION = "reset.process.validation";
-    public static final String TRUE_STRING = "true";
-    public static final String FALSE_STRING = "false";
     public static final Pageable SINGLE_VALUE_PAGE = PageRequest.of(0, 1);
     public static final Pageable FULL_PAGE = PageRequest.of(0, Integer.MAX_VALUE);
     public static final Pageable FULL_PAGE_SORT_BY_RANK = PageRequest.of(0, Integer.MAX_VALUE, Sort.by("rank"));
     public static final Pageable SINGLE_PAGE_SORT_BY_RANK = PageRequest.of(0, 1, Sort.by("rank").descending());
     public static final Pageable TWO_VALUE_PAGE = PageRequest.of(0, 2);
-    public static final String NOT_FOUND = " not found";
     public static final String ALLOWED_CHARS_IN_NAMES = "[[A-Z][a-z][0-9][/][\\s][_][@][#][(][)][.][,]['][-][*][`][/][&]]*";
-    public static final DateTimeFormatter LOCAL_DATE_DD_MM_YYYY
-            = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    public static final String DATE_FORMATE_STRING = "yyyy-MM-dd";
-    public static final DateTimeFormatter LOCAL_DATE_YYYY_MM_DD
-            = DateTimeFormatter.ofPattern(DATE_FORMATE_STRING);
-    public static final DateTimeFormatter LOCAL_DATE_FORMATE
-            = DateTimeFormatter.ofPattern(DATE_FORMATE_STRING);
-    public static final DateTimeFormatter LOCAL_DATE_YYYYMMDD
-            = DateTimeFormatter.ofPattern("yyyyMMdd");
     public final static ContextInfo SUPER_USER_CONTEXT = new ContextInfo(
-            "noreplytestharnesstool@gmail.com",
+            "",
             DEFAULT_SYSTEM_USER_ID,
-            "password",
+            "",
             true,
             true,
             true,
