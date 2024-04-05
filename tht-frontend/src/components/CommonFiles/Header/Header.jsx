d3adb9d53d8c040fa3b462d5c51b533c81afdbd1
Nitin Goyal <ngoyal@argusoft.com>
2024-04-05 13:17:07 +0000

    #408 Initial notification Changes

--- tht-frontend/src/components/CommonFiles/Header/Header.jsx
+++ tht-frontend/src/components/CommonFiles/Header/Header.jsx
@@ -3,6 +3,7 @@ import avatar from "../../../styles/images/defaultDP.jpeg";
 import "./_header.scss";
 import { store } from "../../../store/store";
 import { log_out } from "../../../reducers/authReducer";
+import { BellOutlined, DeleteOutlined } from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { getHighestPriorityRole } from "../../../utils/utils";
@@ -11,6 +12,12 @@ import { DOCUMENT_STATE_ACTIVE } from "../../../constants/document_constants";
 import { DocumentAPI } from "../../../api/DocumentAPI";
 import UseEventEmitter from "../EventEmitter/EventEmitter";
 import { APP_EVENTS } from "../../../constants/event_constants";
+import { NotificationAPI } from "../../../api/NotificationAPI";
+import {
+  NOTIFICATION_STATUS_ARCHIVED,
+  NOTIFICATION_STATUS_UNREAD,
+} from "../../../constants/notification_constants";
+import { notification } from "antd";
 
 /**
  * Header Component
@@ -41,6 +48,11 @@ export default function Header({ headerContent, isSidebarOpen }) {
 
   // Retrieves the user ID from the Redux store.
   const userID = useSelector((store) => store.userInfoSlice.id);
+
+  const [notifications, setNotifications] = useState();
+
+  //useState for the unread notification count
+  const [unreadCount, setUnreadCount] = useState();
 
   // Function: getUserInfo
   // Description: Fetches user information from the Redux store and updates the state with the retrieved data. It also fetches the user's display picture URL from the server.
@@ -73,6 +85,44 @@ export default function Header({ headerContent, isSidebarOpen }) {
       return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
   };
 
+  const unreadNotificationCount = (data) => {
+    const count = data?.filter(
+      (notification) => notification.state == NOTIFICATION_STATUS_UNREAD
+    ).length;
+    setUnreadCount(count);
+  };
+
+  //Function to update the state of a notification
+  const ArchiveNotificationById = async (id) => {
+    try {
+      await NotificationAPI.updateNotificationState(
+        id,
+        NOTIFICATION_STATUS_ARCHIVED
+      );
+      await fetchNotifications(); // Wait for notifications to be fetched after updating state
+    } catch (error) {}
+  };
+
+  //Function to update the state of notifications in bulk
+  const bulkArchiveNotifications = async () => {
+    try {
+      await NotificationAPI.bulkUpdateNotificationState(
+        NOTIFICATION_STATUS_UNREAD,
+        NOTIFICATION_STATUS_ARCHIVED
+      );
+      await fetchNotifications(); // Wait for notifications to be fetched after updating state
+    } catch (error) {}
+  };
+
+  const fetchNotifications = () => {
+    NotificationAPI.getAllNotifications()
+      .then((responseData) => {
+        setNotifications(responseData.data.content);
+        unreadNotificationCount(responseData.data.content);
+      })
+      .catch((error) => {});
+  };
+
   useEffect(() => {
     getUserInfo();
   }, []);
@@ -96,6 +146,10 @@ export default function Header({ headerContent, isSidebarOpen }) {
     };
   }, []);
 
+  useEffect(() => {
+    fetchNotifications();
+  }, []);
+
   return (
     <div id="header" style={{ cursor: "pointer" }}>
       <header>
@@ -107,42 +161,99 @@ export default function Header({ headerContent, isSidebarOpen }) {
           >
             {headerContent}
           </h5>
-          <div className="dropdown">
-            <div
-              className="user-dropdown"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <span className="user-pic">
-                <img src={displayPictureUrl ? displayPictureUrl : avatar} />
-              </span>
-              <span className="user-name">
-                {userInfo?.name}
-                <i className="bi bi-chevron-down"></i>
-              </span>
-              <span style={{fontSize:"12px"}}>
-                {userInfo ? capitalizeFirstLetter(getHighestPriorityRole(userInfo)) : ""}
-              </span>
+          <div className="d-flex">
+            <div className="dropdown">
+              <div
+                className="user-dropdown"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                style={{ position: "relative", display: "inline-block" }}
+              >
+                <div style={{ marginRight: "30px", fontSize: "28px" }}>
+                  <BellOutlined />
+                </div>
+                {unreadCount > 0 ? (
+                  <div className="notification-count">{unreadCount}</div>
+                ) : (
+                  <></>
+                )}
+              </div>
+              {notifications?.length > 0 ? (
+                <ul className="dropdown-menu notification-menu p-3 mt-2">
+                  <h5 className="notification-header">Notifications</h5>
+                  {unreadCount > 0 ? (
+                    <button
+                      className="archive-all-notifications-btn"
+                      onClick={bulkArchiveNotifications}
+                    >
+                      Archive All Notifications
+                    </button>
+                  ) : (
+                    <></>
+                  )}
+
+                  {notifications?.map((notification, index) =>
+                    notification?.state == NOTIFICATION_STATUS_UNREAD ? (
+                      <li
+                        key={notification?.id}
+                        className="list-group-item rounded mt-2 notification-item d-flex"
+                      >
+                        <div>{notification.message}</div>
+                        <div>
+                          <DeleteOutlined
+                            onClick={() => ArchiveNotificationById(notification?.id)}
+                          />
+                        </div>
+                      </li>
+                    ) : (
+                      <></>
+                    )
+                  )}
+                </ul>
+              ) : (
+                <></>
+              )}
             </div>
-            <ul className="dropdown-menu">
-              <li
-                onClick={() => {
-                  navigate("/user-profile");
-                }}
+
+            <div className="dropdown">
+              <div
+                className="user-dropdown"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
               >
-                <a className="dropdown-item">Update Profile</a>
-              </li>
-              <li
-                onClick={() => {
-                  navigate("/login");
-                  dispatch(log_out());
-                }}
-              >
-                <a className="dropdown-item" href="#">
-                  Log Out
-                </a>
-              </li>
-            </ul>
+                <span className="user-pic">
+                  <img src={displayPictureUrl ? displayPictureUrl : avatar} />
+                </span>
+                <span className="user-name">
+                  {userInfo?.name}
+                  <i className="bi bi-chevron-down"></i>
+                </span>
+                <span style={{ fontSize: "12px" }}>
+                  {userInfo
+                    ? capitalizeFirstLetter(getHighestPriorityRole(userInfo))
+                    : ""}
+                </span>
+              </div>
+              <ul className="dropdown-menu">
+                <li
+                  onClick={() => {
+                    navigate("/user-profile");
+                  }}
+                >
+                  <a className="dropdown-item">Update Profile</a>
+                </li>
+                <li
+                  onClick={() => {
+                    navigate("/login");
+                    dispatch(log_out());
+                  }}
+                >
+                  <a className="dropdown-item" href="#">
+                    Log Out
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </header>
