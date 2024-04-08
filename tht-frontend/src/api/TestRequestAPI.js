6ab07e9386b12077a66435cc0b131b84c771b130
Shinu Thomas <shinuk@argusoft.com>
2024-04-08 08:49:02 +0000

    Resolve#406 "Appending message prop on state change api."

--- tht-frontend/src/api/TestRequestAPI.js
+++ tht-frontend/src/api/TestRequestAPI.js
@@ -2,72 +2,74 @@ import { paramSerialize } from "../utils/utils";
 import api from "./configs/axiosConfigs";
 
 export const TestRequestAPI = {
-	createTestRequest: async function (data) {
-		
-			const response = await api.request({
-				url: `/test-request`,
-				method: "POST",
-                data
-			});
-			return response.data;
-		
-	},
-	validateTestRequest: async function (validationTypeKey, data) {
-		
-			const response = await api.request({
-				url: `/test-request/validate`,
-				method: "POST",
-				params: {
-					validationTypeKey
-				},
-				data
-			});
-			return response.data;
-		
-	},
-	getTestRequestsById: async function (testRequestId) {
-		
-			const response = await api.request({
-				url: `/test-request/${testRequestId}`,
-				method: "GET",
-			});
-			return response.data;
-		
-	},
-	getTestRequestsByState: async function (state, sortFieldName, sortDirection, currentPage, pageSize) {
-	
-			const params={}
-			if(!!sortFieldName){
-				params.sort = `${sortFieldName},${sortDirection}`;
-			}
-			if(state) params.state = state;
-			if(currentPage) params.page=currentPage;
-			if(pageSize) params.size=pageSize;
-			const response = await api.request({
-				url: `/test-request`,
-				method: "GET",
-				params,
-				paramsSerializer: params => {
-				  return paramSerialize(params);
-				}
-			});
-			return response.data;
-		
-	},
-	changeState: async function (id, state) {
-		
-			const response = await api.request({
-				url: `/test-request/state/${id}/${state}`,
-				method: "PATCH"
-			});
-			return response.data;
-		
-	},
-	validateChangeState: async function (id, state) {
-			const response = await api.request({
-				url: `/test-request/validate/state/${id}/${state}`,
-				method: "PATCH"
-			});
-			return response.data;	
-	},
+  createTestRequest: async function (data) {
+    const response = await api.request({
+      url: `/test-request`,
+      method: "POST",
+      data,
+    });
+    return response.data;
+  },
+  validateTestRequest: async function (validationTypeKey, data) {
+    const response = await api.request({
+      url: `/test-request/validate`,
+      method: "POST",
+      params: {
+        validationTypeKey,
+      },
+      data,
+    });
+    return response.data;
+  },
+  getTestRequestsById: async function (testRequestId) {
+    const response = await api.request({
+      url: `/test-request/${testRequestId}`,
+      method: "GET",
+    });
+    return response.data;
+  },
+  getTestRequestsByState: async function (
+    state,
+    sortFieldName,
+    sortDirection,
+    currentPage,
+    pageSize
+  ) {
+    const params = {};
+    if (!!sortFieldName) {
+      params.sort = `${sortFieldName},${sortDirection}`;
+    }
+    if (state) params.state = state;
+    if (currentPage) params.page = currentPage;
+    if (pageSize) params.size = pageSize;
+    const response = await api.request({
+      url: `/test-request`,
+      method: "GET",
+      params,
+      paramsSerializer: (params) => {
+        return paramSerialize(params);
+      },
+    });
+    return response.data;
+  },
+  changeState: async function (id, state, message) {
+    const response = await api.request({
+      url: `/test-request/state/${id}/${state}`,
+      method: "PATCH",
+      data: {
+        message: message ? message : null,
+      },
+    });
+    return response.data;
+  },
+  validateChangeState: async function (id, state, message) {
+    const response = await api.request({
+      url: `/test-request/validate/state/${id}/${state}`,
+      method: "PATCH",
+      data: {
+        message: message ? message : null,
+      },
+    });
+    return response.data;
+  },
 };
