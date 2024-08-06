a28b4401fa4774107dd78024d39f4cb9a5069c9b
Bhavim09 <bmehta@argusoft.com>
2024-08-06 16:12:48 +0530

    Change in config file

--- tht-doc/docusaurus.config.ts
+++ tht-doc/docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://DigitalSQR.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ohie-test-harness/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
